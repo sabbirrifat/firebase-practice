90df16b85de38ed130e09a9152d72f328a09306f
Sabbir Rifat <rifat@Sabbirs-iMac.local>
2020-09-15 11:41:34 +0600

    Finish Sign in Sign up auth with firebase

---
+++ src/test.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('dIkBNjYUxFnPq56Lef2v').collection('cartItems').doc('G2xNANA7xVEFSGHAWbwM');
+firestore.doc('/users/dIkBNjYUxFnPq56Lef2v/cartItems/G2xNANA7xVEFSGHAWbwM');
